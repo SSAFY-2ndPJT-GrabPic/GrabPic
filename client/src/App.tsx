25416531dcb4f21341c4f5020fefb70db8708e3e
이해진 <sixb_0417@hanmail.net>
2024-03-04 16:37:23 +0900

    [SETTING] FE 개발 환경 세팅

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Main = styled.div`
+  background-color: #000000;
+  width: 70px;
+  height: 70px;
+  color: #ffffff;
+`
+
+function App() {
+  return (
+    <>
+      <Main className="text-3xl font-bold underline text-yellow-500">test</Main>
+    </>
+  )
+}
+
+export default App
