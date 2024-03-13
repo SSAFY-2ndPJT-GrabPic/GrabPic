e1fc5217efc097f61a13e5a61794e93a44513ef0
이해진 <sixb_0417@hanmail.net>
2024-03-13 17:08:54 +0900

    [FEAT] 메인페이지 구현중

---
+++ client/src/components/Home/UserCard.tsx
@@ -0,0 +1,13 @@
+import * as U from './UserCard.style'
+import React from 'react';
+import { Link } from "react-router-dom";
+
+interface UserCardProps {}
+
+const UserCard: React.FC<UserCardProps> = () => {
+  return (
+    
+  );
+};
+
+export default UserCard;
