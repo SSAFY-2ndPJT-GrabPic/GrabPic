e050b5a1dd8b122c9c24ad11ff01a134c471bf0e
양성규 <yt1356@naver.com>
2024-03-14 16:24:47 +0900

    [Fix] : 이미지 오류 수정 테스트

--- client/src/pages/ResetPw.tsx
+++ client/src/pages/ResetPw.tsx
@@ -3,13 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import ResetPwEmail from '../components/ResetPw/ResetPwEmail';
 import ResetPwCode from '../components/ResetPw/ResetPwCode';
 import ResetPwSet from '../components/ResetPw/ResetPwSet';
+import closeIcon from '../assets/icon/close.png'
 
 export default function ResetPw() {
     return (
     <div className="flex flex-col px-6">
         <div className="flex flex-row items-center self-center mt-5">
         <R.Close to="/login">
-            <img src="src/assets/icon/close.png" alt="" />
+            <img src={closeIcon} alt="" />
         </R.Close>
         <R.ResetPwText>비밀번호 재설정</R.ResetPwText>
         </div>
