abc3c5f56114108bbd425f8d42d33054218aa4ab
박병조 <k97115284@gmail.com>
2024-03-14 17:43:42 +0900

    S10P22D104-166 [FEAT] 지도 컴포넌트 완료 지도 컴포넌트 구현 완료 및 줌인 줌아웃 구현 완료 Closes S10P22D104-166

--- client/src/components/maps/CustomMap.tsx
+++ client/src/components/maps/CustomMap.tsx
@@ -1,42 +1,69 @@
-import { Map } from "react-kakao-maps-sdk"
-import styled from "styled-components";
-import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
-import React from 'react';
-import {useState} from 'react';
+import * as M from "./CustomMap.style"
+import React, { useRef, useState } from 'react';
+
+import { Map, useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
+
+// zoomin 이미지 불러오기
+import plusImg from "../../assets/Map/plus.png";
+// zoomout 이미지 불러오기
+import minusImg from "../../assets/Map/minus.png";
 
 // 위도 경도 프롭처리할 예정.
-// 호출하는 게 
 interface MapsProps {
   lat : number;
   lng : number;
 }
 
-const MapContainer = styled.div`
-  width: 100%;
-  height: calc(100% - 112px); /* 화면 높이에서 112px 제외 */
-`;
-
 const CustomMap: React.FC<MapsProps> = ({ lat, lng }) => {
-  
+  // 지도 호출
   useKakaoLoaderOrigin({
     appkey: "52b3371f40d9c77376d831422bbae913",
     libraries: ["clusterer", "drawing", "services"],
   })
-
+  // 기본 위치(현재 좌표) 호출 및 스타일 지정
   const [state] = useState({
     center: {lat: lat, lng: lng},
-    style: {width: "100%", height: "100%"}
+    style: {width: "100%", height: "100%", Position: "relative", overflow: "hidden"}
   })
+  // 맵 레벨 변경을 위한 선언
+  const mapRef = useRef<kakao.maps.Map>(null)
+
+  const zoomIn = () => {
+    const map = mapRef.current
+    if (!map) return
+    map.setLevel(map.getLevel() - 1)
+  }
+
+  const zoomOut = () => {
+    const map = mapRef.current
+    if (!map) return
+    map.setLevel(map.getLevel() + 1)
+  }
 
   return (
-    <MapContainer>
+    <M.MapContainer>
       <Map // 지도를 표시할 Container
-      id="map"
-      center= {state.center}
-      style={state.style}
-      level={3} // 지도의 확대 레벨
-    />
-    </MapContainer>
+        id="map"
+        center= {state.center}
+        style= {state.style}
+        level={3} // 지도의 확대 레벨
+        ref={mapRef}
+      />
+      <M.Zoom_Control>
+        <M.Zoom_Span onClick={zoomIn}>
+          <M.Zoom_Img
+              src={plusImg}
+              alt="확대"
+          />
+        </M.Zoom_Span>
+        <M.Zoom_Span onClick={zoomOut}>
+          <M.Zoom_Img
+              src={minusImg}
+              alt="축소"
+          />
+        </M.Zoom_Span>
+      </M.Zoom_Control>
+    </M.MapContainer>
   )
 } 
 
