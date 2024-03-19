bd633c37be1e1fe4596ba532f3a6d3c76393d7ad
양성규 <yt1356@naver.com>
2024-03-19 15:31:45 +0900

    [CHORE] : userinfo정보 객체 생성

--- client/src/recoil/atoms/UserState.tsx
+++ client/src/recoil/atoms/UserState.tsx
@@ -1,6 +1,21 @@
 import { atom } from "recoil";
 
+type UserInfoType = {
+  userId: string;
+  email: string;
+  nickName: string;
+  birth: string;
+  gender: string;
+  profilePicture: string;
+  subsCount: number;
+};
+
 export const isLoginState = atom<boolean>({
   key: 'isLogin',
   default: false,
+});
+
+export const userInfo = atom<UserInfoType>({
+  key: 'userInfo',
+  default: {userId: '', email: '', nickName: '', birth: '', gender: '', profilePicture: '', subsCount: 0}
 });
