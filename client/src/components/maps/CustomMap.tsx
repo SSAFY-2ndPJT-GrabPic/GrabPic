2e9973d9e442d1f0f744e66c87101462e3ea270d
박병조 <k97115284@gmail.com>
2024-03-20 14:13:55 +0900

    S10P22D104-171 [FEAT] 핀 리스트 구현 완료(디자인 제외) 현재 더미데이터 발생시키는 기준으로 리스트 구현은 완료되었음. 디자인 포함하여 작업 진행할 예정

--- client/src/components/maps/CustomMap.tsx
+++ client/src/components/maps/CustomMap.tsx
@@ -1,7 +1,7 @@
 import * as M from "./CustomMap.style"
 import React, { useEffect, useRef, useState } from 'react';
 
-import { MapMarker, Map} from "react-kakao-maps-sdk";
+import { MapMarker, Map } from "react-kakao-maps-sdk";
 import useKakaoLoader from "./useKakaoLoader";
 
 // zoomin 이미지 불러오기
@@ -25,28 +25,7 @@ interface ItemProps {
 }
 
 const CustomMap: React.FC = () => {
-  // 지도 호출
   useKakaoLoader();
-    // 랜덤 위도 생성
-  function generateRandomNumberInRange(min: number, max: number): number {
-    return Math.random() * (max - min) + min;
-  }
-
-  function calculateNewCoordinates(currentLat: number, currentLng: number, radius: number): { name:string, lat: number, lng: number, address: string } {
-    // 0~360도 사이에서 랜덤한 각도 생성
-    const randomAngle = generateRandomNumberInRange(0, 360);
-    // 0~반경 사이에서 랜덤한 거리 생성
-    const randomDistance = generateRandomNumberInRange(0, radius);
-  
-    // 새로운 위치의 위도와 경도 계산
-    const lat = currentLat + (randomDistance / 111111) * Math.cos(randomAngle);
-    const lng = currentLng + (randomDistance / (111111 * Math.cos(lat * Math.PI / 180))) * Math.sin(randomAngle);
-    const name = 'testdata';
-    // const address = addressFind(lat, lng);
-    const address = '';
-    return { name, lat, lng, address };
-  }
-
 
   // 현재 좌표 추적 위한 상태
   const [location, setLocation] = useState<MapsProps | null>(null);
@@ -61,9 +40,33 @@ const CustomMap: React.FC = () => {
   // 맵 레벨 변경을 위한 선언
   const mapRef = useRef<kakao.maps.Map>(null);
 
-  const [map] = useState()
-
+  // 마커 리스트
   const [randomCoordinates, setRandomCoordinates] = useState<ItemProps[]>([]);
+
+  // 핀리스트 
+  const [isActive, setActive] = useState<boolean>(false);
+
+  // 필터 활성화
+  const [isClickActive, setClickActive] = useState<boolean[]>([true, false, false]);
+
+  // 랜덤 위도 생성
+  function generateRandomNumberInRange(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+  }
+
+  function calculateNewCoordinates(currentLat: number, currentLng: number, radius: number): { name:string, lat: number, lng: number, address: string } {
+    // 0~360도 사이에서 랜덤한 각도 생성
+    const randomAngle = generateRandomNumberInRange(0, 360);
+    // 0~반경 사이에서 랜덤한 거리 생성
+    const randomDistance = generateRandomNumberInRange(0, radius);
+  
+    // 새로운 위치의 위도와 경도 계산
+    const lat = currentLat + (randomDistance / 111111) * Math.cos(randomAngle);
+    const lng = currentLng + (randomDistance / (111111 * Math.cos(lat * Math.PI / 180))) * Math.sin(randomAngle);
+    const name = 'testdata';
+    const address = '';
+    return { name, lat, lng, address };
+  }
 
   useEffect(() => {
     function getLocation() {
@@ -104,46 +107,29 @@ const CustomMap: React.FC = () => {
       const newCoordinates: ItemProps[] = [];
       for (let i = 0; i < 20; i++) {
         const newCoordinate = calculateNewCoordinates(location.lat, location.lng, 500);
-        randomCoordinates.push(newCoordinate);
-        setRandomCoordinates(newCoordinates)
+        newCoordinates.push(newCoordinate);
       }
+      setRandomCoordinates(newCoordinates);
     }
 
   }, [location])
 
   useEffect(() => {
-    if (!map) return
-    console.log('주소변환')
+    if (!mapRef.current || randomCoordinates.length === 0) return;
+  
+    console.log('주소변환');
     const addressFinder = new kakao.maps.services.Geocoder();
-
-    randomCoordinates.forEach((item, index) => {
+    
+    randomCoordinates.forEach((item) => {
       addressFinder.coord2Address(item.lng, item.lat, (result: any, status: any) => {
         if (status === kakao.maps.services.Status.OK) {
           const address = result[0].address.address_name;
-          randomCoordinates[index].address = address;
-          console.log(address)
+          item.address = address;
         }
       });
     });
-  }, [map]);
+  }, [randomCoordinates]);
 
-  // function addressFind(lat: number, lng: number): string {
-  //   const addressFinder = new kakao.maps.services.Geocoder();
-  //   addressFinder.coord2Address(lng, lat, (result: any, status: any) => {
-  //     if (status === kakao.maps.services.Status.OK) {
-  //       const address = result[0].address.address_name;
-  //       console.log(address)
-  //       return address
-  //     }
-  //   });
-  //   return ''
-  // }
-
-  // 핀리스트 
-  const [isActive, setActive] = useState<boolean>(false);
-
-  // 필터 활성화
-  const [isClickActive, setClickActive] = useState<boolean[]>([true, false, false]);
 
   // 필터를 클릭할 때 호출되는 함수
   const filterChange = (index : number) => {
@@ -152,8 +138,6 @@ const CustomMap: React.FC = () => {
     else if (index === 2) setClickActive([false, false, true])
   }
   
-
-
   // 줌인 함수
   const zoomIn = () => {
     const map = mapRef.current
@@ -180,7 +164,6 @@ const CustomMap: React.FC = () => {
         <MapMarker key={state.center.lat-state.center.lng} position={{ lat: state.center.lat, lng: state.center.lng}} image={{src: myLocateMarker, size: { width: 29, height: 42}}}/>
         {/* locations을 반복하여 각 위치에 마커 생성 */}
           {randomCoordinates.map((location, index) => (
-          
           <MapMarker key={index} position={{ lat: location.lat, lng: location.lng }}/>
         ))}
       </Map>
@@ -211,8 +194,9 @@ const CustomMap: React.FC = () => {
           {randomCoordinates.map((item, index) => (
             <div key={index}>
               <div>Name: {item.name}</div>
-              {/* <div>Latitude: {item.lat}</div>
-              <div>Longitude: {item.lng}</div> */}
+              <div>Latitude: {item.lat}</div>
+              <div>Longitude: {item.lng}</div>
+              <div>address: {item.address}</div>
             </div>
           ))}
         </M.PinList>
@@ -220,6 +204,8 @@ const CustomMap: React.FC = () => {
     </M.MapContainer>
   )
 } 
+
+
 
 export default CustomMap
 
