591f3ac1a06ce027255ce996864ad68350df6937
손동천 <kij8025@naver.com>
2024-03-20 16:38:06 +0900

    [test]test

--- grabpic-BE/src/main/java/org/grabpic/grabpic/encyclopedia/service/EncyclopediaServiceImpl.java
+++ grabpic-BE/src/main/java/org/grabpic/grabpic/encyclopedia/service/EncyclopediaServiceImpl.java
@@ -34,6 +34,6 @@ public class EncyclopediaServiceImpl implements EncyclopediaService{
 
     @Override
     public void collectionRegist(CollectionRegistDTO collectionRegistDTO, String token) {
-
+        
     }
 }
