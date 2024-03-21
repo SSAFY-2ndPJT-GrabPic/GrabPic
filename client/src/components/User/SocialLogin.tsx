8322eb5c572b14bd6160fe4273ace1e77b1aafeb
양성규 <yt1356@naver.com>
2024-03-21 09:57:06 +0900

    [Feat] : 소셜로그인

--- client/src/components/User/SocialLogin.tsx
+++ client/src/components/User/SocialLogin.tsx
@@ -3,26 +3,32 @@ import naverIconUrl from '../../assets/Login/naver.png';
 import kakaoIconUrl from '../../assets/Login/kakao.png';
 
 import * as L from './Login.style';
-import { useEffect } from 'react';
 
 
 export const SocailLogin: React.FC = () => {
-  // const kakaoKey = import.meta.env.VITE_KAKAO_SDK_KEY;
 
-  useEffect(() => {
-  })
+  const url = 'https://j10d104.p.ssafy.io/api';
+
+  const loginClick = (e : number) => {
+    if(e === 1) 
+      window.location.href = url + '/login/oauth2/authorization/google';
+    else if(e === 2)
+      window.location.href = url + '/login/oauth2/authorization/naver';
+    else
+      window.location.href = url + '/login/oauth2/authorization/kakao';
+  }
 
   return (
     <>
-      <L.AnothLogin>
+      <L.AnothLogin onClick={() => {loginClick(1)}}>
         <img src={googleIconUrl} className="mr-3" />
         구글 로그인
       </L.AnothLogin>
-      <L.AnothLogin>
+      <L.AnothLogin onClick={() => {loginClick(2)}}>
         <img src={naverIconUrl} className="mr-3" />
         네이버 로그인
       </L.AnothLogin>
-      <L.AnothLogin>
+      <L.AnothLogin onClick={() => {loginClick(3)}}>
         <img src={kakaoIconUrl} className="mr-3" />
         카카오 로그인
       </L.AnothLogin>
