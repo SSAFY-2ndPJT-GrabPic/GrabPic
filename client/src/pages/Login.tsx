b0a053362b91769eb856155381bc6cc47881ad3f
양성규 <yt1356@naver.com>
2024-03-22 15:15:05 +0900

    [Feat] : 자동 로그인 테스트:

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
 import { BasicLogin } from "../components/User/BasicLogin";
 import { SocailLogin } from "../components/User/SocialLogin";
 
+import { TokenRefresh } from "../api/user";
+
 export const Login: React.FC = () => {
+
+  useEffect(() => {
+    autoCheck();
+  })
+
+  const autoCheck = async () => {
+    await TokenRefresh(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
+  
 
   return (
     <div className="flex flex-col px-6">
