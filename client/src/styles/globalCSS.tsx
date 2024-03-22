0421f427bd0c6ed395b4caaa3acbe5e4c40b950e
양성규 <yt1356@naver.com>
2024-03-22 14:01:41 +0900

    [Feat] : 쿠키 사용 확인

--- client/src/styles/globalCSS.tsx
+++ client/src/styles/globalCSS.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 
 export const InputContainer = styled.div`
     width: 100%;
-    height: 100px;
+    min-height: 100px;
+    max-height: 150px;
     font-family: 'TmoneyR';
     display: flex;
     flex-direction: column;
