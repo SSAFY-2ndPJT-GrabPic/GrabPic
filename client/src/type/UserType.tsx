47115490578b94b45c11bff61c8450011dfbe89e
이해진 <sixb_0417@hanmail.net>
2024-03-25 09:33:23 +0900

    [FIX] user 정보 조회 api 반환 데이터 저장 정보 변경 (userState에 collectCount 추가)

--- client/src/type/UserType.tsx
+++ client/src/type/UserType.tsx
@@ -1,11 +1,21 @@
 
 export type UserInfoType = {
-    userId: number;
-    email: string;
-    nickname: string;
-    name: string;
-    birth: string;
-    gender: string;
-    profilePicture: string;
-    subsCount: number;
-  };
+  userId: number;
+  email: string;
+  nickname: string;
+  name: string;
+  birth: string;
+  gender: string;
+  profilePicture: string;
+  subsCount: number;
+  collectCount: number;
+};
+
+export interface OwnerInfoType {
+  userId: number;
+  nickname: string;
+  gender: string;
+  profilePicture: string;
+  subsCount: number;
+  collectCount: number;
+}
