1f3c91963c2041b96d77cfdc58f9f00d0c75bb43
손동천 <kij8025@naver.com>
2024-03-31 15:28:38 +0900

    [FIX] 객체 설명 분류 추가

--- grabpic-BE/src/main/java/org/grabpic/grabpic/biologyList/db/entity/BiologyListEntity.java
+++ grabpic-BE/src/main/java/org/grabpic/grabpic/biologyList/db/entity/BiologyListEntity.java
@@ -23,7 +23,12 @@ public class BiologyListEntity {
     private String species;
     private String name;
     private String summary;
-    private String content;
+    private String title1;
+    private String content1;
+    private String title2;
+    private String content2;
+    private String title3;
+    private String content3;
     private String imageThumbnail;
     private String imageDetail;
 
