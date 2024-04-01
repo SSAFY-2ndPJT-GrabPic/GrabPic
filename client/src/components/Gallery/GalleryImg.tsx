211432e5a0531c51fbc41d454841eebe26be239e
이해진 <sixb_0417@hanmail.net>
2024-04-01 13:04:46 +0900

    Update client/src/components/Gallery/GalleryImg.tsx

--- client/src/components/Gallery/GalleryImg.tsx
+++ client/src/components/Gallery/GalleryImg.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as G from './GalleryImg.style';
 import { GalleryItemType } from '../../type/GalleryType';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSetRecoilState } from 'recoil';
 import { backState } from '../../recoil/atoms/DetailBackState';
 
