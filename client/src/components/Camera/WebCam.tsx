7596082e2f935a076a0a69ee85894afe627559a7
양성규 <yt1356@naver.com>
2024-04-01 13:41:20 +0900

    [Feat] : 토큰 만료

--- client/src/components/Camera/WebCam.tsx
+++ client/src/components/Camera/WebCam.tsx
@@ -1,24 +1,3 @@
-// const conatiner = {
-//   audio: false,
-//   video: {
-//     facingMode: "environment",
-//     width: {
-//       // min: Math.max(document.documentElement.clientWidth,window.innerWidth),
-//       exact: Math.max(document.documentElement.clientWidth || 0,window.innerWidth || 0),
-//       // max: Math.max(document.documentElement.clientWidth,window.innerWidth),
-//     },
-//     height: {
-//       // min: Math.max(document.documentElement.clientHeight,window.innerHeight),
-//       // ideal: Math.max(document.documentElement.clientHeight,window.innerHeight),
-//       exact: Math.max(document.documentElement.clientHeight || 0,window.innerHeight || 0),
-//       // max: Math.max(document.documentElement.clientHeight,window.innerHeight),
-//     },
-//     frameRate: {
-//       ideal: 60,
-//       min: 30,
-//     },
-//   },
-// };
 
 export class WebCam {
   open = (videoRef: HTMLVideoElement | null, videoWidth : number, videoHeight:number) => {
@@ -27,28 +6,15 @@ export class WebCam {
       navigator.mediaDevices.getUserMedia &&
       videoRef
     ) {
-      // console.log("camera open");
-      console.log(videoWidth);
-      console.log(videoHeight);
-      
 
       navigator.mediaDevices.getUserMedia({
-        video: { facingMode: "environment", width: videoHeight, height:videoWidth,
+        // video: { facingMode: "environment", width: videoHeight, height:videoWidth,
+        video: { facingMode: "environment", width: videoWidth, height:videoHeight,
         frameRate: {
           ideal: 60,
           min: 30,
         },}
       }).then((stream) => {
-        const videoTrack = stream.getVideoTracks()[0];
-        // // 트랙 설정 확인
-        console.log(window.innerWidth);
-        console.log(window.innerHeight);
-        console.log(document.documentElement.clientWidth)
-        console.log(document.documentElement.clientHeight);
-        const settings = videoTrack.getSettings();
-        console.log('폭 (width):', settings.width);
-        console.log('높이 (height):', settings.height);
-        console.log('프레임 속도 (frame rate):', settings.frameRate);
         videoRef.srcObject = stream;
       });
     }
