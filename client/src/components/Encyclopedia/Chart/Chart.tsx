0dc40164b690e153984176030322b4f09e9352e8
이해진 <sixb_0417@hanmail.net>
2024-04-01 12:57:27 +0900

    [FEAT] 차트 페이지 더미 삭제 및 api 연동

--- client/src/components/Encyclopedia/Chart/Chart.tsx
+++ client/src/components/Encyclopedia/Chart/Chart.tsx
@@ -2,108 +2,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import * as C from './Chart.style';
 import cytoscape, { CytoscapeOptions } from 'cytoscape';
 import coseBilkent from 'cytoscape-cose-bilkent';
-// import { getChartList } from '../../../api/encyclopedia';
+import { getChartList } from '../../../api/encyclopedia';
 import { ChartList } from '../../../type/ChartType';
 cytoscape.use(coseBilkent);
-
-interface dummyData {
-  id: number;
-  nodeData: object;
-  edgeData: object;
-}
-
-const dummyData: dummyData = {
-  id: 1,
-  nodeData: {
-    식육목: {
-      id: '식육목',
-      label: '식육목',
-    },
-    고양이: {
-      id: '고양이',
-      label: '고양이',
-    },
-    고양이과: {
-      id: '고양이과',
-      label: '고양이과',
-    },
-    고양이속: {
-      id: '고양이속',
-      label: '고양이속',
-    },
-    개과: {
-      id: '개과',
-      label: '개과',
-    },
-    개속: {
-      id: '개속',
-      label: '개속',
-    },
-    '개(회색늑대)': {
-      id: '개(회색늑대)',
-      label: '개(회색늑대)',
-    },
-    족제비과: {
-      id: '족제비과',
-      label: '족제비과',
-    },
-    족제비속: {
-      id: '족제비속',
-      label: '족제비속',
-    },
-    족재비: {
-      id: '족재비',
-      label: '족재비',
-    },
-  },
-  edgeData: {
-    고양이속간선: {
-      id: '고양이속간선',
-      source: '고양이속',
-      target: '고양이과',
-    },
-    고양이과간선: {
-      id: '고양이과간선',
-      source: '고양이과',
-      target: '식육목',
-    },
-    고양이간선: {
-      id: '고양이간선',
-      source: '고양이',
-      target: '고양이속',
-    },
-    '개(회색늑대)간선': {
-      id: '개(회색늑대)간선',
-      source: '개(회색늑대)',
-      target: '개속',
-    },
-    개속간선: {
-      id: '개속간선',
-      source: '개속',
-      target: '개과',
-    },
-    개과간선: {
-      id: '개과간선',
-      source: '개과',
-      target: '식육목',
-    },
-    족재비간선: {
-      id: '족재비간선',
-      source: '족재비',
-      target: '족제비속',
-    },
-    족제비속간선: {
-      id: '족제비속간선',
-      source: '족제비속',
-      target: '족제비과',
-    },
-    족제비과간선: {
-      id: '족제비과간선',
-      source: '족제비과',
-      target: '식육목',
-    },
-  },
-};
 
 interface data {
   data: {
@@ -119,24 +20,22 @@ interface ChartProps {
   userId: number;
 }
 
-// const Chart: React.FC<ChartProps> = ({ userId }) => {
-const Chart: React.FC<ChartProps> = () => {
+const Chart: React.FC<ChartProps> = ({ userId }) => {
   const chartRef = useRef<HTMLDivElement | null>(null);  // <C.Chart> 컴포넌트 참조
   const data: data[] = [];   // node + edge 데이터 담을 리스트
   const [chartList, setChartList] = useState<ChartList>()
 
   // Chart데이터 받아오기
   useEffect(() => {
-    // console.log(userId)
-    // getChartList(
-    //   userId,
-    //   (res) => {
-    //     setChartList(res.data)
-    //     console.log(res.data)
-    //   },
-    //   (err) => { console.error(err) }
-    // )
-    setChartList(dummyData)
+    console.log(userId)
+    getChartList(
+      userId,
+      (res) => {
+        setChartList(res.data)
+        console.log(res.data)
+      },
+      (err) => { console.error(err) }
+    )
   }, [])
   
   // nodeData와 edgeData에 대한 value를 data 리스트에 담는 작업
