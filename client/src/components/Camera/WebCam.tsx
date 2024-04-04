aa3eee5951418446f477975280e9eadb26144cbf
양성규 <yt1356@naver.com>
2024-04-04 12:09:11 +0900

    [Fix] : 카메라 권한 수정

--- client/src/components/Camera/WebCam.tsx
+++ client/src/components/Camera/WebCam.tsx
@@ -1,26 +1,49 @@
-
 export class WebCam {
-  open = async (videoRef: HTMLVideoElement | null, videoWidth : number, videoHeight:number) => {
+  open = async (
+    videoRef: HTMLVideoElement | null,
+    videoWidth: number,
+    videoHeight: number
+  ) => {
     if (
       navigator.mediaDevices &&
       navigator.mediaDevices.getUserMedia &&
       videoRef
     ) {
-
       const devices = await navigator.mediaDevices.enumerateDevices();
-      const cameras = devices.filter(device => device.kind === "videoinput" && device.label.includes("back"));
+      const cameras = devices.filter(
+        (device) =>
+          device.kind === 'videoinput' && device.label.includes('back')
+      );
+
       const index = cameras.length - 1;
-      const deviceId = cameras[index].deviceId
-      navigator.mediaDevices.getUserMedia({
-        video: { facingMode: "environment", width: videoHeight, height:videoWidth,deviceId: { exact: deviceId},
-        // video: { facingMode: "environment", width: videoWidth, height:videoHeight,deviceId: { exact: deviceId},
-        frameRate: {
-          ideal: 60,
-          min: 30,
+      const deviceId = index >= 0 ? cameras[index].deviceId : null; 
+
+      const info = {
+        video: {
+          facingMode: 'environment',
+          width: videoHeight,
+          height: videoWidth,
+          deviceId: { exact: deviceId },
+          frameRate: {
+            ideal: 60,
+            min: 30,
+          },
         },
-        }
-        
-      }).then((stream) => {
+      };
+
+      const info2 = {
+        video: {
+          facingMode: 'environment',
+          width: videoHeight,
+          height: videoWidth,
+          frameRate: {
+            ideal: 60,
+            min: 30,
+          },
+        },
+      };
+
+      navigator.mediaDevices.getUserMedia(deviceId?info:info2).then((stream) => {
         videoRef.srcObject = stream;
       });
     }
