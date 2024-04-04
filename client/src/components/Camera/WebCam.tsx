47178515d71a0eb41a146f3c8a4e863f4aa2d4d0
양성규 <yt1356@naver.com>
2024-04-04 12:43:29 +0900

    [Test] : 줌 테스트

--- client/src/components/Camera/WebCam.tsx
+++ client/src/components/Camera/WebCam.tsx
@@ -22,8 +22,8 @@ export class WebCam {
       const info = {
         video: {
           facingMode: 'environment',
-          width: videoWidth,
-          height: videoHeight,
+          width: videoHeight,
+          height: videoWidth,
           deviceId: { exact: deviceId },
           zoom: true,
           advanced: [{ zoom: zoom }],
